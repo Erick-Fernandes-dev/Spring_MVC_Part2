8385c4330ef69fd472021b2638c75e2f60d942d9
Erick-Fernandes-dev <erick.farias@dcx.ufpb.br>
2022-06-08 17:33:34 -0300

    Cpnsertando error no Spring Security

---
+++ mudi/src/main/java/br/com/algnologia/mvc/mudi/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package br.com.algnologia.mvc.mudi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.servlet.*;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	//configuração de autorização
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeHttpRequests()
+			.anyRequest().authenticated()
+		.and()
+		.formLogin(form -> form
+	            .loginPage("/login")
+	            .permitAll()
+	        );
+			
+	}
+	
+	@Bean//autenticando usuario
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("jose")
+				.password("jose")
+				.roles("ADM")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+
+}
