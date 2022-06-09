48839dab93986f006a6201b813c39b342d0aeae5
Erick-Fernandes-dev <erick.farias@dcx.ufpb.br>
2022-06-09 17:54:56 -0300

    Chore: Spring security and user authentication with JDBC authentication

--- mudi/src/main/java/br/com/algnologia/mvc/mudi/WebSecurityConfig.java
+++ mudi/src/main/java/br/com/algnologia/mvc/mudi/WebSecurityConfig.java
@@ -1,7 +1,11 @@
 package br.com.algnologia.mvc.mudi;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +17,9 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DataSource dataSource;
 
 	//configuração de autorização
 	@Override
@@ -25,7 +32,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.formLogin(form -> form
 				.loginPage("/login")
 				.permitAll()
-		);
+		)
+		.logout(logout -> logout.logoutUrl("/logout"));
+	}
+	
+	//Utilizando o JDBC authentication para poder autenticar o usuário
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.jdbcAuthentication()
+			.dataSource(dataSource);
 	}
 
 	//autenticando usuario
